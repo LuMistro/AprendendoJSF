5eab8e630e231ff6498e4621d5b8464568890fa7
Luiza Mistro <luiza.wmistro@gmail.com>
2019-10-02 19:07:19 -0300

    Listar finalizado

--- src/main/java/sc/senac/br/cadastropessoa/dao/PessoaDao.java
+++ src/main/java/sc/senac/br/cadastropessoa/dao/PessoaDao.java
@@ -1,7 +1,11 @@
 package sc.senac.br.cadastropessoa.dao;
 
+import net.bytebuddy.implementation.bind.annotation.Super;
 import sc.senac.br.cadastropessoa.model.Pessoa;
 
 public class PessoaDao extends GenericDao<Pessoa, Integer> {
+	public PessoaDao() {
+		super(Pessoa.class);
+	}
 
 }
